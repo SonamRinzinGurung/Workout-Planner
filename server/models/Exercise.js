80ca5f0db0de36440c988adb5c6620b7c5020a0d
Sonam Rinzin Gurung <sonamrinzingurung@gmail.com>
2024-01-08 13:40:55 +0545

    Workout and Exercise models

---
+++ server/models/Exercise.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const exerciseSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    sets: {
+      type: Number,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    weight: {
+      type: Number,
+      required: false,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
