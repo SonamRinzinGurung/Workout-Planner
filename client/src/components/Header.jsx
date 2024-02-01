614fe0fcfe78effdc64d72f2d6837b7495ffdcad
Sonam Rinzin Gurung <sonamrinzingurung@gmail.com>
2024-02-01 13:48:25 +0545

    Logout button and functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoIosMenu, IoIosCloseCircleOutline } from "react-icons/io";
-import { MenuLinks } from ".";
+import { MenuLinks, Button } from ".";
 import { ToastContainer } from "react-toastify";
 
 const Header = () => {
   const modalRef = useRef();
   const [darkMode, setDarkMode] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = useCallback(() => {
     setMenuOpen((prev) => !prev);
@@ -41,6 +42,12 @@ const Header = () => {
     setDarkMode(!darkMode);
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+    toggleMenu();
+  };
+
   return (
     <>
       <nav className="p-2 shadow-sm flex justify-center items-center">
@@ -51,7 +58,7 @@ const Header = () => {
                 ref={modalRef}
                 className="modal-content border-none shadow-lg relative w-full h-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current dark:bg-gray-700 dark:text-gray-100 "
               >
-                <div className="modal-header flex flex-col flex-shrink-0  items-start p-4 border-b border-gray-200 rounded-t-md">
+                <div className="modal-header flex justify-between p-4 border-b border-gray-200 rounded-t-md">
                   <button
                     fontSize="large"
                     color="error"
@@ -60,6 +67,13 @@ const Header = () => {
                   >
                     <IoIosCloseCircleOutline className="text-red-700 w-6 h-6  hover:text-red-900 transition ease-in-out duration-300" />
                   </button>
+                  <div>
+                    <Button
+                      name="Logout"
+                      className="border hover:bg-slate-500 hover:shadow-md hover:text-gray-100 transition duration-300"
+                      handleClick={handleLogout}
+                    />
+                  </div>
                 </div>
                 <div className="modal-body relative p-8 px-10 flex flex-col h-3/4 gap-5 items-center">
                   <MenuLinks handleClick={toggleMenu} />
